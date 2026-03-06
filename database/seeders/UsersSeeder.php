f4a8d27ceae45ac4dd30d0e6a289cc216ea57b37
Luis Navarro <lm395746@gmail.com>
2026-03-06 14:10:36 -0400

    agregar seeder de permisos

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -13,26 +13,31 @@ class UsersSeeder extends Seeder
      * Run the database seeds.
      */
     public function run(): void
-    {
-        User::create([
-            'name' => 'luis navarro',
-            'email' => 'lnavarro@mercal.gob.ve' ,
-            'password' => bcrypt('12345678'),   
-        ]);
-        User::create([
-            'name' => 'kleinys prieto',
-            'email' => 'kprieto@mercal.gob.ve' ,
-            'password' => bcrypt('12345678'),   
-        ]);
-        User::create([
-            'name' => 'moises castillo',
-            'email' => 'moicastillo@mercal.gob.ve' ,
-            'password' => bcrypt('admin123'),   
-        ]);
-        User::create([
-            'name' => 'danyerbert brito',
-            'email' => 'dan@mercal.gob.ve' ,
-            'password' => bcrypt('12345678'),   
-        ]);
-    }
+        {
+                
+                User::create([
+                    'name' => 'luis navarro',
+                    'email' => 'lnavarro@mercal.gob.ve' ,
+                    'password' => bcrypt('12345678'),   
+                ])->assignRole('admin');
+
+                
+                User::create([
+                    'name' => 'kleinys prieto',
+                    'email' => 'kprieto@mercal.gob.ve' ,
+                    'password' => bcrypt('12345678'),   
+                ])->assignRole('admin'); 
+
+                User::create([
+                    'name' => 'moises castillo',
+                    'email' => 'moicastillo@mercal.gob.ve' ,
+                    'password' => bcrypt('admin123'),   
+                ])->assignRole('admin'); 
+
+                User::create([
+                    'name' => 'danyerbert brito',
+                    'email' => 'dan@mercal.gob.ve' ,
+                    'password' => bcrypt('12345678'),   
+                ])->assignRole('admin'); 
+        }
 }
